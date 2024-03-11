47b334a7cfa2b551ee155d67e6c9954aa877f494
bidya7890 <162495053+bidya7890@users.noreply.github.com>
2024-03-11 14:45:05 +0530

    02_datatypes.js

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all js as newer versions
+//alert(3+3)
+
+// we are usingt nodejs, not browser
+
+console.log(3+3)
+console.log("Bidya")
+let name="bidya"
+let age =18
+let isLoggedIn= true
+let state;
+let temprature = null;
+
+//-----primitive datatypes--//
+// number => 2 to the power
+//bigint
+//string=> ""
+//boolean=> true/false
+//null=> standalone value
+//undefined=> values not assigned
+//symbol => uniqueness
+
+//---object--//
+ console.log(typeof "bidya");
+ console.log(typeof null); //null is an object
+ console.log(typeof undefined); //undefined is a type
+
