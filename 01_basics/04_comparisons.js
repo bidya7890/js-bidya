23c9571ba588045680bf409f0600817e72313709
bidya7890 <162495053+bidya7890@users.noreply.github.com>
2024-03-12 16:32:12 +0530

    Create 04_comparisons.js

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,31 @@
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2!=1)
+// console.log(2==2)
+
+// console.log("2">1)
+// console.log("02">1)
+
+//output is true,true
+
+// console.log(null >0) 
+// console.log(null==0) 
+// console.log(null >=0) //js converts null to 0  in line 13
+//output is false,false, true not gives the expected value ,creates confusion
+
+// console.log(undefined==0)
+// console.log(undefined>0)
+// console.log(undefined<0)
+//output is false,false,false
+
+//=== strict check
+
+// console.log("2"===2)
+//output is false as strict checks both the values and datatypes
+
+
+
+
+
+
+
