c626e17f67fe9c842f97fc54c892962c825b2ab8
bidya7890 <162495053+bidya7890@users.noreply.github.com>
2024-03-12 16:51:43 +0530

    datatypes_summary.js

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,9 @@
+// primitive (call by value) changes in the copy
+
+// 7 categories: Strings , Number,Boolean, null,undefined,Symbol,BigInt
+
+// Reference (Non primitive)
+
+// Arrays, Objects, Functions
+
+//Js is dynamically typed language
