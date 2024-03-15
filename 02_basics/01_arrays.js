aaabb32be46e166459c23418b17e0e5ca8a477ab
bidya7890 <162495053+bidya7890@users.noreply.github.com>
2024-03-15 19:01:26 +0000

    array part 1

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -40,6 +40,86 @@ console.log(myArr)
 myArr.pop() // to remove the last value of the array
 console.log(myArr)
 
+myArr.unshift(9)
+console.log(myArr)
+
+// 9 is inserted at the start of the array, value gets shifted like suppose there are 10,000 values , optimisation is less
+
+myArr.shift()
+
+console.log(myArr)
+
+// 9 is removed from the beggining of the array
+
+console.log(myArr.includes(9))
+
+// in this array 9 is included or not
+
+console.log(myArr.indexOf(9))
+
+// value is -1 as 9 is not in the array
+
+console.log(myArr.indexOf(3))
+
+//index is 3
+
+const newArr = myArr.join()
+
+// adds all the element in the array as string
+
+console.log(newArr)
+
+//output is 0,1,2,3,4,5 , datatype is string type
+
+console.log(typeof newArr)
+
+
+// slice , splice (IMPORATANT)
+
+console.log("A", myArr)
+
+//output is A [ 0, 1, 2, 3, 4, 5 ]
+
+const myn1=myArr.slice(1,3)
+
+
+
+// 1 and 2 is included but 3 is excluded
+
+// slice returns a section of array
+
+console.log(myn1)
+//output is [ 1, 2 ]
+
+console.log("B", myArr)
+
+// output is B [ 0, 1, 2, 3, 4, 5 ]
+
+const myn2=myArr.splice(1,3)
+console.log("C",myArr)
+
+//Here the original value is changed as output is C [ 0, 4, 5 ]
+console.log(myn2)
+//output is [1,2,3]
+
+// here in splice last number is included, and yes components of arrays get manipualted hence should use slice instead of splice
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
