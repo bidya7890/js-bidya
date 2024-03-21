a0a83951f09cc91c4810445670788f0568351b88
BIDYASHREE ADITYA <162495053+bidya7890@users.noreply.github.com>
2024-03-21 19:05:09 +0000

    functions basics

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -160,6 +160,8 @@ console.log(ins)
 
 
 
+
+
  
 
 
---
+++ 03_basics/01_functions.js
@@ -0,0 +1,139 @@
+
+
+
+function sayMyName(){
+console.log("B")
+console.log("I")
+console.log("D")
+console.log("Y")
+console.log("A")
+
+}
+
+//sayMyName()
+
+//() known as execution or 
+
+//out put is B
+//I
+//D
+//Y
+//A
+
+function addTwoNumbers(number1,number2){
+    console.log(number1+number2)
+
+}
+
+//addTwoNumbers(3,2)
+
+//ouput is Nan (non a number if arguements are not given)
+// Now output is 5
+
+//addTwoNumbers(3,"4")
+//output is 34
+
+//addTwoNumbers(3,"a")
+//output is 3a
+
+//(number1,number2) parameters, when values are passed they are known as arguements
+
+//addTwoNumbers(3,5)
+
+//now i can save this value in variable
+
+//const result=addTwoNumbers(3,5)
+//console.log(result)
+//result value is undefined
+
+//here functions doesnt return the value to console
+
+
+
+function addTwoNumbers1(num1,num2){
+    // let result1=num1+num2 // inner scope
+    // return result1
+
+    return num1+num2
+}
+
+const result1=addTwoNumbers1(3,5) //outer scope
+//console.log("result1: ", result1)
+
+//output is result1:  8
+
+function logInUserMessage(userName = "sam"){ // so that the value will never be undefined
+    if (userName ===undefined || !userName ){
+        console.log("please enter a username")
+        return // not to go the the next scope that is not to return just logged in so the output will be please ebter a username , undefined
+
+    }
+    return`${userName} just Logged In` 
+}
+// (logInUserMessage("bidya"))
+
+//nothing is returned as we didnt write console 
+
+//console.log(logInUserMessage("bidya"))
+
+//output is bidya just logged in
+
+//console.log(logInUserMessage(""))
+
+//output is just logged in
+
+//console.log(logInUserMessage("bidya"))
+// if nothing is passed we get value as undefined
+
+function calculateCartPrice(val1,val2,...num1){
+
+    return num1
+
+}
+console.log(calculateCartPrice(200,2000,3000))
+
+// ... rest operator that is how many values are there give them all in one bundle 
+//output is [ 200, 2000, 3000 ]
+
+//fucntion mein multiple values passed by rest operator
+
+//after using val1,val2 before num1 and rest operator he values 200 and 2000 are stored in those varaibles hence 3000 is only returned
+
+const user ={
+    userName: "bidya",
+    price:199
+}
+
+//TO PASS THE OBJECT IN FUNCTION
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.userName} and the price is ${anyObject.price}`)
+
+
+}
+
+
+
+//handleObject(user)
+
+handleObject({
+    userName:"sam",
+    price:399
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
