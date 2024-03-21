f4ca97c67fe2ad10c223e06f38f4ffda5c78848a
BIDYASHREE ADITYA <162495053+bidya7890@users.noreply.github.com>
2024-03-21 16:20:31 +0000

    objects part 2

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -53,14 +53,67 @@ const obj4={5:"a",6:"b"}
 
 // same problems as arrays
 
-const obj3=Object.assign({},obj1,obj2,obj4)
-console.log(obj3)
+//const obj3=Object.assign({},obj1,obj2,obj4)
+const obj3={...obj1,...obj2}
+//console.log(obj3)
 
 //{} optional parameter known as the target and obj1,obj2,obj4 acts as source, gives the exact value. Assign= it is known as object assign, returns modify target object with merging
 // If we dont use {}, the values will go to obj1, so for better syntacally properly we should use {}
 //output is { '1': 'a', '2': 'b', '3': 'a', '4': 'b' }
 
 // end at 11:09, video number 17
+
+//continuation of video
+
+const users = [
+    {
+        id:1,
+        email:"h@gmail.com"
+    },
+    {
+
+    },
+    {}
+]
+
+users[1].email
+console.log(tinderUser)
+
+//to give the values of all the keys
+
+
+console.log(Object.keys(tinderUser))
+
+// output is[ 'id', 'name', 'isLoggedIn' ]
+
+// to know the values
+console.log(Object.values(tinderUser))
+
+//output is [ '123abc', 'Sammy', false ]
+
+console.log(Object.entries(tinderUser))
+
+// It gives keys and its respective values in nested arrays as follows
+//[ [ 'id', '123abc' ], [ 'name', 'Sammy' ], [ 'isLoggedIn', false ] ]
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'))
+
+//output is true, sometimes in objects there are nested loops, and to get a certain values we hae to go through from these nested loops to get a value hence to check weather that value exists or not we check with the hasOwnProperty
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
 
 
 
